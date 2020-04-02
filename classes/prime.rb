8a0eb92ee7fea55099266e852e39e4ab4404fb65
Michael <michael.ginalick@gmail.com>
2020-04-02 17:24:49 -0500

    changed variable and class attribute names to be more descriptive

--- classes/prime.rb
+++ classes/prime.rb
@@ -1,23 +1,23 @@
 require_relative '../errors/invalid_number_error.rb'
 
 class Prime
-  attr_reader :number
+  attr_reader :max_primes
   attr_writer :result
   
-  def initialize(number)
-    raise InvalidNumberError.new("Please provide a positive number") unless number > 0
-    @number = number
+  def initialize(max_primes)
+    raise InvalidNumberError.new("Please provide a positive number") unless max_primes > 0
+    @max_primes = max_primes
     @result = []
   end
 
   def calculate_primes
-    return @result if number < 2
-    i = 2
+    return @result if max_primes < 2
+    num = 2
     
     loop do
-      isPrime(i)
-      break if @result.length == number
-      i.even? ? i+=1 : i+=2
+      isPrime(num)
+      break if @result.length == max_primes
+      num.even? ? num+=1 : num+=2
     end
     @result
   end
