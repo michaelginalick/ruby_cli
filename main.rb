fcb62a6ba3ff6460887b16b4a336a6b4e9919e39
Michael <michael.ginalick@gmail.com>
2020-04-02 19:40:37 -0500

    add main file and parse args from command line

---
+++ main.rb
@@ -0,0 +1,29 @@
+require 'pry'
+require 'optparse'
+require_relative './errors/invalid_number_error.rb'
+system "clear"
+
+options = {}
+OptionParser.new do |parser|
+  parser.banner = "Usage: main.rb [options]"
+
+  parser.on("--help", "--help", "") do |_e|
+    options[:help] = true
+    puts parser
+  end
+
+  parser.on("--prime", "--prime PRIME", String, "Will calculate the number of primes.") do |prime|
+    options[:prime] = prime
+  end
+
+end.parse!
+
+if options[:help].nil?
+  begin
+    arg = ARGV
+    raise InvalidNumberError.new("Please provide a valid number greater than 0") unless arg.length > 0 && arg[0].to_i > 0
+    GenerateSequences.new(arg[0].to_i, options).generate
+  rescue => exception
+    puts "#{exception}"
+  end
+end
